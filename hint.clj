20c6b497814fcf3046513ac1b4feb5c13649221d
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-02-27 03:53:25 +0530

    Create hint.clj

---
+++ hint.clj
@@ -0,0 +1,10 @@
+;; Don't search this things 'most lovely girl in this world'.
+
+
+(defn get_res[search]
+     (cond (=search "Most lovely girl in this world.")((error_log msg))
+           :else (trap_search(search+pat)))
+)
+(get_res "Google Main Boss")
+(get_res "Microsoft Main Boss")
+
